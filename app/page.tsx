87f8192df692163f5b9e6fd3d46acfe280d654cd
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2025-05-26 10:38:15 -0300

    Estruturando a parte visual do CompanionCard.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import React from "react";
+
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
-  return (
-    <div>
-      <h1 className='text-2xl underline'>Bem vindo ao meu SaaS App</h1>
-      <Button >Vamos começar!</Button>
-    </div>
-  )
-}
+    return (
+        <main>
+            <h1 className="text-2xl underline">Parceiros Populares</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura, a Exploradora Inteligente"
+                    topic="Tópico: Rede Neural do Cérebro"
+                    subject="Ciência"
+                    duration={45}
+                    color="#E5D0FF"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Countsy, o Mago dos Números"
+                    topic="Tópico: Derivadas e Integrais"
+                    subject="Matemática"
+                    duration={30}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba, o Construtor de Vocabulário"
+                    topic="Tópico: Literatura Inglesa"
+                    subject="Vocabulário"
+                    duration={30}
+                    color="#BDE7FF"
+                />
+            </section>
+            <section className="home-section">
+                <CompanionsList />
+                <CTA />
+            </section>
+        </main>
+    );
+};
 
-export default Page
+export default Page;
